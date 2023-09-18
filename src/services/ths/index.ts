5f6740e2e3dbf237fc0f21f97fcb940d9fffe2bd
Paul Gung <gongshaoxu@gmail.com>
2023-09-18 21:07:41 +0800

    feat: add www

--- src/services/ths/index.ts
+++ src/services/ths/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const protocal = window.location.protocol;
 const baseUrl =
-  protocal === 'http:' ? `${protocal}//1.12.67.43/ths` : `${protocal}//paulgung.online/ths`;
+  protocal === 'http:' ? `${protocal}//1.12.67.43/ths` : `${protocal}//www.paulgung.online/ths`;
 
 //获取组合信息
 export async function getCombinationData(params?: { [key: string]: any }) {
