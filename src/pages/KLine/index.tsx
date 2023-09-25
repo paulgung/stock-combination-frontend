5e458f022a44857e75c9489084c10e95260bb27d
Paul Gung <gongshaoxu@gmail.com>
2023-09-25 16:22:56 +0800

    feat: 支持港美绿涨红跌

--- src/pages/KLine/index.tsx
+++ src/pages/KLine/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom'; // version 5.2.0
 // 由于HXKline依赖于HXKlineChart，所以此处需要先引用HXKlineChart(会自动挂载到window)，如仍报错无法找到HXKlineChart，请使用`HXKline.setHXKlineChart(HXKlineChart)`方式关联
-import ErrorBoundary from '@/services/ErrorBoundary';
 import HXKlineChart from '@ths-m/HXKlineChart';
 
 import HXKline from '@ths-m/HXKline/lib/HXKline.common';
@@ -25,10 +24,27 @@ type StockListItem = {
   turnover: number;
 };
 
+// 香港、美股采用绿涨红跌
+const specialRegion = ['HK', 'US'];
+
+const getBrowserLocale = () => {
+  const browserLocale = navigator.language || navigator.userLanguage;
+  const [languageCode, regionCode] = browserLocale.split('-');
+  return { languageCode, regionCode };
+};
+
 const Index: React.FC = () => {
   const { sc: stockCode, mid: marketId } = useParams(); // 获取父组合id
 
   useEffect(() => {
+    // 获取地域码
+    const { regionCode } = getBrowserLocale();
+
+    if (!specialRegion.includes(regionCode)) {
+      HXKlineChart.overrideDefaultConstants('DEFAULT_COLOR_UP', '#26A69A');
+      HXKlineChart.overrideDefaultConstants('DEFAULT_COLOR_DOWN', '#EF5350');
+    }
+
     HXKline.setHXKlineChart(HXKlineChart);
 
     // K线图
@@ -41,26 +57,49 @@ const Index: React.FC = () => {
     });
     const chart = klineChart.getChart();
     chart.createIndicator('VOL', false);
-    chart.createIndicator('MACD', false);
+    // chart.createIndicator(
+    //     'MACD',
+    //     false,
+    // );
+
+    const trendChart = HXKline.init(
+      'trend-chart',
+      {
+        stockCode,
+        marketId,
+        chartType: 'trend',
+        timePeriod: 'min_1',
+      },
+      {
+        dateFormat: 'HH:mm:ss',
+        styles: {},
+      },
+    );
   }, []);
 
   return (
-    <ErrorBoundary>
+    <div
+      style={{
+        padding: '0',
+        height: '100%',
+      }}
+    >
       <div
+        id="kline-chart"
         style={{
-          padding: '0',
-          height: '100%',
+          width: '100%',
+          height: '80%',
         }}
-      >
-        <div
-          id="kline-chart"
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-        ></div>
-      </div>
-    </ErrorBoundary>
+      ></div>
+      <div
+        id="trend-chart"
+        style={{
+          width: '100%',
+          height: '19%',
+          borderTop: '1px solid #ccc',
+        }}
+      ></div>
+    </div>
   );
 };
 export default Index;
